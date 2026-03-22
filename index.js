a8db528817779797e88958a741b956e5278cc969
KotikGamerz <162711066+KotikGamerz@users.noreply.github.com>
2026-03-22 18:40:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+require('dotenv').config();
+const { Client } = require('discord.js-selfbot-v13');
 
+const client = new Client();
+
+client.on('ready', () => {
+    console.log(`✅ Залогинен как ${client.user.tag}`);
+});
+
+client.login(process.env.USER_TOKEN);
